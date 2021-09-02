d2e81a775382587653f02538b30da29dd39d1c81
Darsh Shah <darsh.shah@olethia.com>
2021-09-02 20:04:43 +0530

    Initial Commit

---
+++ interQuestions.dart
@@ -0,0 +1,27 @@
+void main() {
+ List l1 = [4, 8, 33, 55, 7, 99, 21];
+  List l2 = [18, 4, 17, 65, 99, 20, 21];
+  List l3 = [90, 39, 6, 7, 21, 99, 26];
+  
+  // Output  (Common elements in l1,l2, l3)
+  // [7, 99, 21]   
+  // 5 pts  - 10 mins
+  
+  Map<int, String> a = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f"}; 
+  
+  // Output (Split the Map such that the Resultant is the List of elements having even MapKeys values)
+  // [{2:'b'},{4:'d'},{6:'f'}]
+  // 5 pts - 10 mins
+  
+  
+  List ids = ['1234', '3455', '2330', '1111'];
+  List selectedUsers = [{"id": '1234', "name": "Darsh"}, {"id": '3455', "name": "Ashish"}, {'id': "1212", 'name': 'Raj'}];
+  
+  // Output (Print the List of ids which are present in both the Lists)
+  // ['1234', '3455']
+  // 5 pts - 10 mins
+  
+  
+  
+  
+}
