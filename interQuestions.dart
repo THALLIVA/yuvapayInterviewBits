230a5b06478837a888cf85a26a8964af3c0cc08e
Darsh Kirtikumar Shah <darshshah1996@gmail.com>
2021-09-20 18:22:40 +0530

    Update interQuestions.dart
    Update

--- interQuestions.dart
+++ interQuestions.dart
@@ -25,3 +25,34 @@ void main() {
   
   
 }
+
+
+void main() {
+  
+  List list1 = [1,2,3];
+  List list2 = ["a","b","c"];
+  
+  // Desired Output
+
+  //[{1,'a'}, {2,'b'}, {3,'c'}]
+  // 10pts - 15mins
+  
+  Map employee = {"employees":[  
+    {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},  
+    {"name":"Bob", "email":"bob32@gmail.com"},  
+    {"name":"Darsh", "email":"darshshah1996@gmail.com"}]};
+  
+  // Would can we print Jai's Email by iterating the List? 
+  // Can you convert it in a Model ? 
+  // 10pts - 15mins
+  
+  List l1 = [4, 8, 33, 55, 7, 99, 21];
+  List l2 = [18, 4, 17, 65, 99, 20, 21];
+  List l3 = [90, 39, 6, 7, 21, 99, 26];
+  
+  // Output  (Common elements in l1,l2, l3)
+  // [99, 21]   
+  // 5 pts  - 10 mins
+  
+}
+
